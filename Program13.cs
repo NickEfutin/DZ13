154ef8faa63ec800696f0a38307c758f1263781c
Nick <efutin.kolya@yandex.ru>
2022-09-22 22:47:21 +0300

    done

--- Program13.cs
+++ Program13.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число.");
+int input = Convert.ToInt32(Console.ReadLine());
+    if (input>99)
+        Console.WriteLine("Третья цифра "+input.ToString()+" = "+input.ToString()[2]);
+    else if (input<100)
+        Console.WriteLine("Третьей цифры нет");
